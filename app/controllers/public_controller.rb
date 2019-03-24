8e5603003585506248f40c7e483ca0d0337f5081
Brett Clanton <brettclanton001@protonmail.com>
2019-03-24 10:47:53 -0400

    basic layout in place

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,18 @@
 class PublicController < ApplicationController
 
   def home
+    redirect_to music_path
+  end
+
+  def music
+  end
+
+  def videos
+  end
+
+  def schedule
+  end
+
+  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
   root to: 'public#home'
+  get '/music', to: 'public#music'
+  get '/videos', to: 'public#videos'
+  get '/schedule', to: 'public#schedule'
+  get '/contact', to: 'public#contact'
 
 end
