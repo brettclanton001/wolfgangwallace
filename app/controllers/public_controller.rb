97e4833a4c9efa9e0134a61faecad3bcfba55e0f
Brett Clanton <brettclanton001@protonmail.com>
2019-04-05 20:45:43 -0400

    move data out of settings and add day of week to schedule

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -5,14 +5,25 @@ class PublicController < ApplicationController
   end
 
   def music
+    @soundcloud_tracks = load_data_file :soundcloud_tracks
   end
 
   def videos
+    @facebook_videos = load_data_file :facebook_videos
   end
 
   def schedule
+    @scheduled_shows = load_data_file :scheduled_shows
   end
 
   def contact
+  end
+
+  private
+
+  def load_data_file(name)
+    file_path = File.join(Rails.root, 'data_files', "#{name}.yml")
+    file = File.read(file_path)
+    YAML.load(file)
   end
 end
